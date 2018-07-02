487ff0ca6ef76cf68f098c7ebe6cd4823da844fc
José Esteves <ajesteves@gmail.com>
2018-07-02 17:58:13 -0300

    first step

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,37 @@
-import "phoenix_html"
+import "phoenix_html";
 
-import React from 'react'
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../css/phoenix.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/phoenix.css";
 
-const HelloReact = (props) => (
-  <div>HELLO REACT!</div>
-)
+import { InputText } from "./components/inputs";
 
-ReactDOM.render(<HelloReact />, document.getElementById("my-react-app"))
+const Form = props => (
+  <form>
+    <InputText id="colegio" label="Colegio" placeholder="Nombre del Colegio" />
+    <InputText
+      id="cantidad"
+      label="Cantidad"
+      placeholder="Cantidad de Alumnos"
+    />
+    <InputText
+      id="representante"
+      label="Representante"
+      placeholder="¿ Con quien hablamos ?"
+    />
+    <button type="submit" className="btn btn-success">
+      Siguiente
+    </button>
+  </form>
+);
+
+ReactDOM.render(<Form />, document.getElementById("my-react-app"));
+
+//     <div className="form-check">
+// <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+// <label className="form-check-label" for="exampleCheck1">
+//   Check me out
+// </label>
+// </div>
---
+++ assets/js/components/inputs.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const InputText = props => (
+  <div className="form-group">
+    <label htmlFor={props.id}>{props.label}</label>
+    <input
+      type={props.type || "text"}
+      className="form-control"
+      id={props.id}
+      aria-describedby="emailHelp"
+      placeholder={props.placeholder}
+    />
+    <small id="emailHelp" className="form-text text-muted">
+      {props.helpText}
+    </small>
+  </div>
+);
