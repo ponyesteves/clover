c37a4ebb0615fba8c01b715a5c01ae8475c69b12
José Esteves <ajesteves@gmail.com>
2018-07-06 13:30:59 -0300

    temp

--- assets/js/containers/step_two.js
+++ assets/js/containers/step_two.js
@@ -1,21 +1,34 @@
-import React from "react"
-import { GenericInput } from "../components/input"
-import { connect } from "react-redux"
+import React from 'react'
+import { GenericInput } from '../components/input'
+import { connect } from 'react-redux'
 const PreStepTwoForm = props => (
   <div>
     <div key={1}>{props.lead.colegio}</div>
     <form key={2}>
       <div className="container">
         <h1>Elija la combinación que más le guste...</h1>
-
-        <label className="radio-inline">
-          <input type="radio" name="survey" id="Radios1" value="Yes" />
-          Yes
-        </label>
-        <label className="radio-inline">
-          <input type="radio" name="survey" id="Radios2" value="No" />
-          No
-        </label>
+        <div className="option_row">
+          <div className="option">
+            <label htmlFor="exampleRadios1">
+              Buzo
+            </label>
+            <input
+              type="radio"
+              name="buzo_campera"
+              value="buzo"
+            />
+          </div>
+          <div className="option">
+            <label htmlFor="exampleRadios1">
+              Campera
+            </label>
+            <input
+              type="radio"
+              name="buzo_campera"
+              value="campera"
+            />
+          </div>
+        </div>
       </div>
 
       <button type="submit" className="btn btn-success">
