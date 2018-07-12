9462297afbbf7c00a100b936191d6167d03c71d8
José Esteves <ajesteves@gmail.com>
2018-07-12 10:15:56 -0300

    beta

--- assets/js/components/pedido.js
+++ assets/js/components/pedido.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const prePedido = (props) => (
-  <div className="container">
-  <div className="pedido_row">
+const prePedido = props => (
+  <div
+    className="pedido_row"
+    style={{ display: (props.step == 'one' && 'none') || 'block' }}
+  >
     <div>Cantidad: {props.cantidad}</div>
     <div>Precio Unitario: {props.precio_unitario}</div>
     <div>Total: {props.precio_total}</div>
-    </div>
-    </div>
+  </div>
 )
 
 const mapStateToProps = state => ({
+  step: state.step,
   cantidad: state.lead.cantidad,
   precio_unitario: state.pedido.precio_unitario,
   precio_total: state.pedido.precio_total
-
 })
+
 export const Pedido = connect(mapStateToProps)(prePedido)
