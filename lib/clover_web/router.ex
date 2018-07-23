8bc200f5fe2912021246a9dbb6bdadbf4e9e3586
José Esteves <ajesteves@gmail.com>
2018-07-23 14:36:07 -0300

    login router

--- lib/clover_web/router.ex
+++ lib/clover_web/router.ex
@@ -40,7 +40,8 @@ defmodule CloverWeb.Router do
   scope "/", CloverWeb do
     # Use the default browser stack
     pipe_through([:browser])
-    resources("/sessions", SessionController, only: [:new, :create])
+    get("login", SessionController, :new)
+    resources("/sessions", SessionController, only: [:create])
     delete("/sessions/drop", SessionController, :drop)
   end
 
