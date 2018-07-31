733c67f375da2bc9105145de4b5e8450860924ba
José Esteves <ajesteves@gmail.com>
2018-07-31 10:13:33 -0300

    change Precios

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,20 +1,19 @@
 export const app_base = '/presup'
 
 export const precios_base = [
-  { max: 25, precio: 1800 },
-  { max: 40, precio: 1700 },
-  { max: 60, precio: 1600 },
-  { max: 200, precio: 1500 }
+  { max: 25, precio: 1750 },
+  { max: 40, precio: 1650 },
+  { max: 200, precio: 1550 }
 ]
 
 export const buzo_campera = {
   buzo: 0,
-  campera: 150
+  campera: 50
 }
 
 export const remera_chomba_nada = {
   no: 0,
-  remera: 400,
+  remera: 420,
   chomba: 600
 }
 
@@ -26,13 +25,13 @@ export const capucha = {
 
 export const colores = {
   uno: 0,
-  dos: 150,
-  tres: 150
+  dos: 100,
+  tres: 200
 }
 
 export const bordados = {
   hasta_tres: 0,
-  hasta_seis: 150
+  hasta_seis: 80
 }
 
 export const all = {...buzo_campera, ...remera_chomba_nada, ...capucha, ...colores, ...bordados}
